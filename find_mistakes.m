fdb73924d3f06c94e3226d2e1375a08725176241
shota-inamura <shota.inamura0213@gmail.com>
2018-01-14 21:55:11 +0900

    first commit

---
+++ find_mistakes.m
@@ -0,0 +1,29 @@
+clear all
+%画像を読み込む
+img = imread('img1.jpg');
+%余白を取り除く
+[h,w,~] = size(img);
+c = 37;
+img = imcrop(img,[c 0 w-c*2-3 h]);
+imshow(img)
+%画像aと画像bに分割する
+[h,w,~] = size(img);
+img_a = imcrop(img,[0 0 w/2-2 h]);
+img_b = imcrop(img,[w/2+1 0 w/2-3 h]);
+figure;imshow(img_a)
+figure;imshow(img_b)
+
+%形の差異を求める
+figure;imshowpair(img_a,img_b)
+figure;imshowpair(img_a,img_b,'diff')
+
+%画像aと画像bのRGB値と求める
+a_r = img_a(:,:,1);
+a_g = img_a(:,:,2);
+a_b = img_a(:,:,3);
+b_r = img_b(:,:,1);
+b_g = img_b(:,:,2);
+b_b = img_b(:,:,3);
+%色の差異を求める
+img_d = (a_r==b_r & a_g==b_g & a_b==b_b);
+figure;imshow(img_d)
